2a630f3e46d80416c14c07f5e7ad0ea6bc3be516
Xwilarg <xwilarg@protonmail.com>
2025-09-28 00:39:46 +0200

    Add noise to doors

--- Assets/Scripts/Prop/Door.cs
+++ Assets/Scripts/Prop/Door.cs
@@ -1,9 +1,34 @@
+using EdmontonJam.Manager;
+using EdmontonJam.SO;
 using UnityEngine;
 
 namespace EdmontonJam.Prop
 {
     public class Door : MonoBehaviour
     {
+        [SerializeField]
+        private NoiseInfo _noiseInfo;
 
+        private Rigidbody _rb;
+        private float _noiseTimer;
+
+        private void Awake()
+        {
+            _rb = transform.GetChild(0).GetComponentInChildren<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (_noiseTimer > 0f)
+            {
+                _noiseTimer -= Time.deltaTime;
+            }
+            else if (_rb.angularVelocity.magnitude > 0f)
+            {
+                _noiseTimer = .5f;
+
+                NoiseManager.Instance.SpawnNoise(transform.position, _noiseInfo);
+            }
+        }
     }
 }
