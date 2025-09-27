a98d879c7a9303dbc28d91f6dacd8be6c8dafc5b
Xwilarg <xwilarg@protonmail.com>
2025-09-28 01:41:09 +0200

    Fix door colliders

--- Assets/Scripts/Prop/Door.cs
+++ Assets/Scripts/Prop/Door.cs
@@ -1,16 +1,23 @@
 using EdmontonJam.Manager;
+using EdmontonJam.Noise;
 using EdmontonJam.SO;
 using UnityEngine;
 
 namespace EdmontonJam.Prop
 {
-    public class Door : MonoBehaviour
+    public class Door : MonoBehaviour, INoiseMaker
     {
         [SerializeField]
         private NoiseInfo _noiseInfo;
 
         private Rigidbody _rb;
         private float _noiseTimer;
+
+        private float _disllowEmitTimer;
+        public void StunNoiseReporting(float time)
+        {
+            _disllowEmitTimer = time;
+        }
 
         private void Awake()
         {
@@ -19,11 +26,16 @@ namespace EdmontonJam.Prop
 
         private void Update()
         {
+            if (_disllowEmitTimer > 0f)
+            {
+                _disllowEmitTimer -= Time.deltaTime;
+            }
+
             if (_noiseTimer > 0f)
             {
                 _noiseTimer -= Time.deltaTime;
             }
-            else if (_rb.angularVelocity.magnitude > ResourceManager.Instance.GameInfo.MinDoorMagnitudeForNoise)
+            else if (_rb.angularVelocity.magnitude > ResourceManager.Instance.GameInfo.MinDoorMagnitudeForNoise && _disllowEmitTimer <= 0f)
             {
                 _noiseTimer = .5f;
 
