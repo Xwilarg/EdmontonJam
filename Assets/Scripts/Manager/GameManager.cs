86dd5ef99ccfeb0d500643e116da2e82ea1a15f1
Xwilarg <xwilarg@protonmail.com>
2025-09-27 16:20:12 +0200

    Add sample prop

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -5,8 +5,14 @@ namespace EdmontonJam.Manager
 {
     public class GameManager : MonoBehaviour
     {
+        public static GameManager Instance { private set; get; }
+
+        public static bool IsChasing { private set; get; }
+
         private void Awake()
         {
+            Instance = this;
+
             Translate.Instance.SetLanguages(new string[] { "english", "french" });
         }
     }
