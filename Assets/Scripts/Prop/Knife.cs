a8d62620008afa164e622e28d21b6068636cfefe
Nour-alshehadeh <nuralskore@gmail.com>
2025-09-28 11:13:59 -0600

    update6

---
+++ Assets/Scripts/Prop/Knife.cs
@@ -0,0 +1,16 @@
+using System.Security;
+using UnityEngine;
+namespace EdmontonJam.Prop
+{
+    public class Knife : MonoBehaviour
+    {
+        private void Update()
+        {
+            
+        }
+
+    }
+
+}
+
+
