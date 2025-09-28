20ef662f6064e69a9a573a09bea88e6141f457fa
Xwilarg <xwilarg@protonmail.com>
2025-09-28 21:09:17 +0200

    Add minimap

--- Assets/Scripts/Minimap/SetAllToMat.cs
+++ Assets/Scripts/Minimap/SetAllToMat.cs
@@ -5,11 +5,21 @@ namespace EdmontonJam.Minimap
     public class SetAllToMat : MonoBehaviour
     {
         [SerializeField]
-        private Material _mat;
+        private Material _matWall, _matFloor;
 
         private void Awake()
         {
-            
+            foreach (var r in GetComponentsInChildren<Renderer>())
+            {
+                if (r.gameObject.name.Contains("wall", System.StringComparison.InvariantCultureIgnoreCase))
+                {
+                    r.material = _matWall;
+                }
+                else
+                {
+                    r.material = _matFloor;
+                }
+            }
         }
     }
 }
