4cb214c89f14a17e39b94ed16752716d2dc7f2f5
Xwilarg <xwilarg@protonmail.com>
2025-09-28 20:59:02 +0200

    Update map

---
+++ Assets/Scripts/Minimap/SetAllToMat.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace EdmontonJam.Minimap
+{
+    public class SetAllToMat : MonoBehaviour
+    {
+        [SerializeField]
+        private Material _mat;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
